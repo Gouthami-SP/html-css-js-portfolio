bcb45312d9d8109ebfb0051ffd2c0370130c7148
Gouthami-SP <gouthamiprasanna76@gmail.com>
2025-01-03 16:49:24 +0530

    counter

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const navLinks = document.querySelector('.nav-links');
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
 }
+
+
+const counterValue = document.getElementById('counter-value');
+
+let visitCount = localStorage.getItem('visitCount');
+
+
+if (visitCount === null) {
+    visitCount = 1;
+} else {
+    visitCount = parseInt(visitCount) + 1;
+}
+
+
+localStorage.setItem('visitCount', visitCount);
+
+
+counterValue.textContent = visitCount;
+
